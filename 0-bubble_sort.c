8d9c877646b0f7482af2cfa6186d08ea28dba1b4
King-AJr <talk2king.aj@gmail.com>
2023-01-24 01:24:53 -0800

    .

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -29,10 +29,11 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+
+				print_array(array, size);
 				flag = 0;
 			}
 		}
-		print_array(array, size);
 		b--;
 	}
 }
